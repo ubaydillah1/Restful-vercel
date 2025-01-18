f6b7d757a9b467a9a648d37b63cf74fa87b2a4b1
ubaydillah1 <ubaydillah1737@gmail.com>
2025-01-18 15:18:42 +0700

    add cors

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 require("dotenv").config();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: "*",
+  })
+);
 
+app.use(express.json());
 const bookRouter = require("./routes/book.router");
 
 app.use("/api", bookRouter);
 
 app.listen(process.env.PORT, () =>
-  console.log("Server is running on port 5000")
+  console.log("Server is running on port 3000")
 );
